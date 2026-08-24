73fa6c07fcd6af10ed4c98e800d154d7299b845a
gitfath <ciferulquiora07@gmail.com>
2026-08-24 17:24:42 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -14,7 +14,8 @@ from typing import List, Dict
 
 try:
     from dotenv import load_dotenv
-    load_dotenv()
+    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
 except ImportError:
     print("⚠️ python-dotenv non installé.")
 
